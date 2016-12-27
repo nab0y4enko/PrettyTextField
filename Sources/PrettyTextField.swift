f83d55e9381e8d83b95cdd2ada5f7d383662fc74
Oleksii Naboichenko <naboichenko@zfort.com>
2016-12-27 20:30:32 +0200

    change names for padding variables

--- Sources/PrettyTextField.swift
+++ Sources/PrettyTextField.swift
@@ -10,25 +10,59 @@ import UIKit
 
 @IBDesignable public class PrettyTextField: UITextField {
     
-    // MARK: - Public Properties
-    @IBInspectable public var textTopPadding: CGFloat = 0
-    @IBInspectable public var textLeftPadding: CGFloat = 0
-    @IBInspectable public var textBottomPadding: CGFloat = 0
-    @IBInspectable public var textRightPadding: CGFloat = 0
-    
-    // MARK: - Computed Properties
-    private var textPaddings: UIEdgeInsets {
-        return UIEdgeInsets(top: textTopPadding, left: textLeftPadding, bottom: textBottomPadding, right: textRightPadding)
+    // MARK: - Properties for Interface Builder
+    @available(*, unavailable, message: "This property is reserved for Interface Builder. Use 'textInset' instead.")
+    @IBInspectable public var textTopInset: CGFloat {
+        get {
+            return textInset.top
+        }
+        set {
+            textInset.top = newValue
+        }
     }
     
+    @available(*, unavailable, message: "This property is reserved for Interface Builder. Use 'textInset' instead.")
+    @IBInspectable public var textLeftInset: CGFloat {
+        get {
+            return textInset.left
+        }
+        set {
+            textInset.left = newValue
+        }
+    }
+    
+    @available(*, unavailable, message: "This property is reserved for Interface Builder. Use 'textInset' instead.")
+    @IBInspectable public var textBottomInset: CGFloat {
+        get {
+            return textInset.bottom
+        }
+        set {
+            textInset.bottom = newValue
+        }
+    }
+
+    @available(*, unavailable, message: "This property is reserved for Interface Builder. Use 'textInset' instead.")
+    @IBInspectable public var textRightInset: CGFloat {
+        get {
+            return textInset.right
+        }
+        set {
+            textInset.right = newValue
+        }
+    }
+    
+    // MARK: - Public Properties
+    var textInset: UIEdgeInsets = UIEdgeInsets()
+    
     // MARK: - UITextField
+    
     /// Placeholder position
     public override func textRect(forBounds bounds: CGRect) -> CGRect {
-        return UIEdgeInsetsInsetRect(bounds, textPaddings)
+        return UIEdgeInsetsInsetRect(bounds, textInset)
     }
     
     /// Text position
     public override func editingRect(forBounds bounds: CGRect) -> CGRect {
-        return UIEdgeInsetsInsetRect(bounds, textPaddings)
+        return UIEdgeInsetsInsetRect(bounds, textInset)
     }
 }
