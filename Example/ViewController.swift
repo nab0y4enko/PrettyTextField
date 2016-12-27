ab97c52fb104f8f51b2fe20ae2ce12fc5b08b030
Oleksii Naboichenko <naboichenko@zfort.com>
2016-12-27 20:29:51 +0200

    added PrettyPasswordTextField and Example target

---
+++ Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Oleksii Naboichenko on 12/27/16.
+//  Copyright © 2016 Oleksii Naboichenko. All rights reserved.
+//
+
+import UIKit
+import PrettyTextField
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
+
---
+++ Sources/PrettyPasswordTextField.swift
@@ -0,0 +1,13 @@
+//
+//  PrettyPasswordTextField.swift
+//  PrettyTextField
+//
+//  Created by Oleksii Naboichenko on 12/27/16.
+//  Copyright © 2016 Oleksii Naboichenko. All rights reserved.
+//
+
+import UIKit
+
+class PrettyPasswordTextField: PrettyTextField {
+
+}
